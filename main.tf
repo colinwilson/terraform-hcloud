fcba3ef6511342eae0ce07994505436d9f9d7266
Colin Wilson <colin.wilson@protonmail.ch>
2020-06-13 16:03:10 +0100

    switch from ansible to basic cloud-init

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "hcloud_ssh_key" "default" {
   name       = var.ssh_public_key_name
-  public_key = file(var.ssh_public_key)
+  public_key = var.ssh_public_key
 }
 
 resource "hcloud_network" "default" {
@@ -30,55 +30,21 @@ resource "hcloud_server" "server" {
   location    = each.value.location
   backups     = each.value.backups
   ssh_keys    = [var.ssh_public_key_name]
+  user_data   = templatefile("${path.module}/user_data.yaml", {
+    floating_ip = hcloud_floating_ip.default.ip_address
+  })
 
   provisioner "remote-exec" {
-    inline = [var.install_ansible_dependencies ? var.ansible_dependencies_install_command : "sleep 0"]
+    inline = [
+      var.run_rancher_deploy ? "cloud-init status --wait --long > /dev/null && ${var.rancher_node_command} ${each.value.roles} --internal-address ${each.value.private_ip_address}" : "",
+      "service networking restart"
+    ]
 
     connection {
       host        = self.ipv4_address
       type        = "ssh"
       user        = "root"
-      private_key = file(var.ssh_private_key)
-    }
-  }
-
-  provisioner "ansible" {
-    ansible_ssh_settings {
-      insecure_no_strict_host_key_checking = true
-    }
-
-    plays {
-      enabled = var.run_ansible_playbook
-
-      playbook {
-        file_path = var.ansible_playbook_path
-      }
-
-      extra_vars = {
-        cluster_name = var.cluster_name
-        floating_ip  = hcloud_floating_ip.default.ip_address
-        server_name  = each.value.name
-        ansible_user = "root"
-      }
-
-      vault_id = [var.ansible_vault_password_path]
-    }
-
-    connection {
-      host = self.ipv4_address
-      type = "ssh"
-      user = "root"
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [var.run_rancher_deploy ? "${var.rancher_node_command} ${each.value.roles} --internal-address ${each.value.private_ip_address}" : ""]
-
-    connection {
-      host        = self.ipv4_address
-      type        = "ssh"
-      user        = var.post_ansible_ssh_user
-      private_key = file(var.ssh_private_key)
+      private_key = var.ssh_private_key
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -4,14 +4,12 @@ variable "cluster_name" {}
 variable "servers" {}
 variable "ssh_private_key" {}
 variable "ssh_public_key" {}
-variable "ansible_playbook_path" {}
-variable "ansible_vault_password_path" {}
 variable "rancher_node_command" {}
 
 # Optional configuration
 
 variable "hcloud_location" {
-  default = "nbg1"
+  default = "hel1"
 }
 variable "private_ip_range" {
   default = "10.0.0.0/16"
@@ -27,18 +25,6 @@ variable "private_network_zone" {
 }
 variable "floating_ip_name" {
   default = "default"
-}
-variable "install_ansible_dependencies" {
-  default = true
-}
-variable "ansible_dependencies_install_command" {
-  default = "sudo yum install -y python36"
-}
-variable "run_ansible_playbook" {
-  default = false
-}
-variable "post_ansible_ssh_user" {
-  default = "deploy"
 }
 variable "run_rancher_deploy" {
   default = true
