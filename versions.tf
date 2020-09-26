bb3559f134125504c7d59c3f255dec148e0b770f
Colin Wilson <colin.wilson@protonmail.ch>
2020-09-26 06:36:37 +0100

    update required provider

--- versions.tf
+++ versions.tf
@@ -2,6 +2,9 @@ terraform {
   required_version = ">= 0.12.7, < 0.14"
 
   required_providers {
-    hcloud = ">= 1.21.0"
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.21.0"
+    }
   }
 }
